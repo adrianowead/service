f8e7db0aeebdd2f48698351003b0e8f85c964ef9
Adriano Maciel <adriano_mail@hotmail.com>
2021-08-05 14:08:58 -0300

    Update service.go

--- service.go
+++ service.go
@@ -18,7 +18,7 @@
 //	import (
 //		"log"
 //
-//		"github.com/kardianos/service"
+//		"github.com/adrianowead/service"
 //	)
 //
 //	var logger service.Logger
@@ -59,7 +59,7 @@
 //			logger.Error(err)
 //		}
 //	}
-package service // import "github.com/kardianos/service"
+package service // import "github.com/adrianowead/service"
 
 import (
 	"errors"
